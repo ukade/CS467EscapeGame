be0bf418b0929cd989b8e9d5cd5a76290d0dfee4
Larisa Xie <xiela@oregonstate.edu>
2025-07-06 18:36:54 -0700

    Added FirstPersonController

---
+++ CS467EscapeGame/Assets/FirstPersonController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    [Header("Movement Speeds")]
+    [SerializeField] private float walkSpeed = 6.0f;
+
+    [Header("Look Parameters")]
+    [SerializeField] private float mouseSensitivity = 0.1f;
+    [SerializeField] private float upDownLookRange = 80f;
+
+    [Header("References")]
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private PlayerInputHandler playerInputHandler;
+
+    private Vector3 currentMovement;
+    private float verticalRotation;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private Vector3 CalculateWorldDirection()
+    {
+        Vector3 inputDirection = new Vector3(playerInputHandler.MovementInput.x, 0f, playerInputHandler.MovementInput.y);
+        Vector3 worldDirection = transform.TransformDirection(inputDirection);
+        return worldDirection.normalized;
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 worldDirection = CalculateWorldDirection();
+        currentMovement.x = worldDirection.x * walkSpeed;
+        currentMovement.z = worldDirection.z * walkSpeed;
+
+        characterController.Move(currentMovement * Time.deltaTime);
+
+    }
+
+    private void ApplyHorizontalRotation(float rotationAmount)
+    {
+        transform.Rotate(0, rotationAmount, 0);
+    }
+
+    private void ApplyVerticalRotation(float rotationAmount)
+    {
+        verticalRotation = Mathf.Clamp(verticalRotation - rotationAmount, -upDownLookRange, upDownLookRange);
+        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+
+    private void HandleRotation()
+    {
+        float mouseXRotation = playerInputHandler.RotationInput.x * mouseSensitivity;
+        float mouseYRotation = playerInputHandler.RotationInput.y * mouseSensitivity;
+
+        ApplyHorizontalRotation(mouseXRotation);
+        ApplyVerticalRotation(mouseYRotation);
+    }
+}
+
+// How to Make a FIRST PERSON CONTROLLER in Unity uploaded by Brockosh
+// Accessed 7/6/2025 
+// https://www.youtube.com/watch?v=vBWcb_0HF1c
---
+++ CS467EscapeGame/Assets/PlayerInputHandler.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    [Header("Input Action Asset")]
+    [SerializeField] private InputActionAsset playerControls;
+
+    [Header("Action Map Name Reference")]
+    [SerializeField] private string actionMapName = "Player";
+
+    [Header("Action Name References")]
+    [SerializeField] private string movement = "Movement";
+    [SerializeField] private string rotation = "Rotation";
+
+    private InputAction movementAction;
+    private InputAction rotationAction;
+
+    public Vector2 MovementInput {  get; private set; }
+    public Vector2 RotationInput { get; private set; }
+
+    private void Awake()
+    {
+        InputActionMap mapReference = playerControls.FindActionMap(actionMapName);
+
+        movementAction = mapReference.FindAction(movement);
+        rotationAction = mapReference.FindAction(rotation);
+
+        SubscribeActionValuesToInputEvents();
+
+    }
+
+    private void SubscribeActionValuesToInputEvents()
+    {
+        movementAction.performed += inputInfo => MovementInput = inputInfo.ReadValue<Vector2>();
+        movementAction.canceled += inputInfo => MovementInput = Vector2.zero;
+
+        rotationAction.performed += inputInfo => RotationInput = inputInfo.ReadValue<Vector2>();
+        rotationAction.canceled += inputInfo => RotationInput = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        playerControls.FindActionMap(actionMapName).Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.FindActionMap(actionMapName).Disable();
+    }
+}
+
+// How to Make a FIRST PERSON CONTROLLER in Unity uploaded by Brockosh
+// Accessed 7/6/2025
+// https://www.youtube.com/watch?v=vBWcb_0HF1c
