7a6c6a587fe5e69709d7fa073be36075defc2078
Katrine Chow <katrine.chow@gmail.com>
2025-07-22 19:33:12 -0700

    Quit Script Test in Build
    Added Quit Script to each scene to enable quitting upon hitting 'esc' key anywhere, including while in Editor's Play Mode

--- CS467EscapeGame/Assets/Scripts/QuitScript.cs
+++ CS467EscapeGame/Assets/Scripts/QuitScript.cs
@@ -5,22 +5,31 @@ using System.Collections;
 
 public class Quit : MonoBehaviour
 {
-   public void QuitGame()
+/*   public void QuitGame()
 {
-    #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-    #else
-        Application.Quit();
-    #endif
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Below code enables "quitting" while in Editor's Play Mode. Though it doesn't seem to work hmm...
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
 }
-
+*/
     //Below Update function checks if user clicks Esc key. If yes, quit the game
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            //Below code enables "quitting" while in Editor's Play Mode. Though it doesn't seem to work hmm...
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
             Application.Quit();
+#endif
+            //Application.Quit();
         }
     }
 }
