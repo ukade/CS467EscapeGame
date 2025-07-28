1cbb6d84b7c78e2da6cf9c010e1fc0fbbcdcd1ae
warnerni <warnerni@oregonstate.edu>
2025-07-27 19:09:41 -0700

    Adding in a script to track the status of the player.  Set to where certain events will only trigger when the

--- CS467EscapeGame/Assets/Nick's Scripts/Break Rocks.cs
+++ CS467EscapeGame/Assets/Nick's Scripts/Break Rocks.cs
@@ -7,11 +7,20 @@ using UnityEngine;
 public class BreakRocks : MonoBehaviour
 {
     public bool pickaxe_check;
-    public string interact_check;
+    public GameObject camper;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    /* Used the following sources in developing the folowing code 
+    https://docs.unity3d.com/ScriptReference/Component.GetComponent.html
+    https://docs.unity3d.com/Manual/class-GameObject.html#AccessingOtherGameObjects
+    https://docs.unity3d.com/Manual/class-GameObject.html
+    This is not an exhaustive list. Please see the attached README for a full list of sources. 
+    */
+
     void Start()
     {
         Debug.Log("Script Started");
+        camper = GameObject.Find("Player Flags");
     }
 
     // Update is called once per frame
@@ -19,17 +28,25 @@ public class BreakRocks : MonoBehaviour
     /* This programming was developed and adapted from the sources at these two links:
     https://docs.unity3d.com/ScriptReference/KeyCode.html
     https://docs.unity3d.com/ScriptReference/Input.html
+    https://docs.unity3d.com/Manual/class-GameObject.html
+    https://docs.unity3d.com/ScriptReference/Component.GetComponent.html
+
+    It appears to me the only parameter that works for GetComponent is the name of the component with no spaces. 
+    I see an example of this in the last source above but did not see where it was mentioned in any other sources. 
     Please see the README for a full list of sources.
     */
     void Update()
     {
-        if (interact_check == "yes"){
+        pickaxe_check = camper.GetComponent<PlayerFlags>().has_pickaxe;
+        if (pickaxe_check == true){
 
             Debug.Log("yep");
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Destroy(gameObject);
+        
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(gameObject);
+   
+            }
         }
     }
 }
