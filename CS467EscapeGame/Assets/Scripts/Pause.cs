4e12ccb7d6717a30306742b45651d6cdf0bcd574
Katrine Chow <katrine.chow@gmail.com>
2025-08-02 16:32:21 -0700

    Update on Pause.cs citations
    Cited sources for pausing game

--- CS467EscapeGame/Assets/Scripts/Pause.cs
+++ CS467EscapeGame/Assets/Scripts/Pause.cs
@@ -12,6 +12,8 @@ public class Pause : MonoBehaviour
 
     void Update()
     {
+        //Sourced from Brian Swanson's OpenDoor.cs script to make cursor visible when TimesUp
+        //GameObject is active
         if (TimesUp.activeInHierarchy)
         {
             Cursor.visible = true;
@@ -22,6 +24,9 @@ public class Pause : MonoBehaviour
 
     public void Continue()
     {
+        //Referenced and adapted from the following YouTube tutorial:
+        //BMo [BMoDev]. (2020, May 18). 6 Minute PAUSE MENU Unity Tutorial. https://www.youtube.com/watch?v=9dYDBomQpBQ
+        //YouTube. Accessed 8/2/2025.
         Debug.Log("In Continue");
         TimesUp.SetActive(false);
         Time.timeScale = 1f;
