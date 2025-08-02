bd52120311e9a612bc767bf4ebe9b9222ce95c1f
warnerni <warnerni@oregonstate.edu>
2025-08-02 10:36:13 -0700

    Started development on the bucket script for Larisa's pedestal puzzle idea.

---
+++ CS467EscapeGame/Assets/Room 3 Scripts/Puzzle 3/BucketScript.cs
@@ -0,0 +1,42 @@
+/* Some of the code below is generated automatically when Unity creates a MonoBehavior script. It has been adapted to work for our teams project. 
+Some sources are included below because they are particularly relevant but they are not meant to be an exhaustive list. 
+The below code was built after learning from/adapted from code from several different sources. 
+Please see the attached README for a full list of cited sources. 
+*/
+
+using UnityEngine;
+
+public class BucketScript : MonoBehaviour
+{
+    /* Built after learning from/adapted from these relevant sources:
+    https://docs.unity3d.com/Manual/class-GameObject.html
+    https://docs.unity3d.com/ScriptReference/GameObject.html
+    Please see the attached README for a full list of cited sources.
+    */
+    public GameObject pondCollider;
+    public GameObject campfireCollider;
+    public GameObject bucketWater;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // On trigger enter here
+    //https://docs.unity3d.com/ScriptReference/Collider.OnTriggerEnter.html
+
+
+
+
+
+
+
+
+}
